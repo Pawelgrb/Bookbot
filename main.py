674da805d67ec6f8d79e6c60605f16ffffbaa012
Pavel019 <pawel.mrk.grb@gmail.com>
2025-07-07 18:04:10 +0100

    creating a func in stats.py that counts char

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_words
+from stats import char_count
 
 
 #function that reads the file
@@ -10,11 +11,14 @@ def get_book_text(path_file):
 
 
 def main():
-    # get the file path
+    # get the file path and calculation from stats.py
     book = get_book_text("books/frankenstein.txt")
     words = book.split()
     num_words = count_words(words)
+    characters = char_count(book)
     print(num_words, "words found in the document")
+    print(characters)
+    
 
  
 main()
--- stats.py
+++ stats.py
@@ -8,3 +8,13 @@ def count_words(words):
         count += 1  
 
     return count
+
+def char_count(strings):
+    char = {}
+    texts= strings.lower()
+    for text in texts:
+        if text in char:
+            char[text] +=1
+        else:
+            char[text] = 1
+    return char
