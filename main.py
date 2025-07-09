34dda2dbb5ad55ceb859ccd66357ca2d12839e39
Pavel019 <pawel.mrk.grb@gmail.com>
2025-07-09 15:12:37 +0100

    project finished, added a filter for symbols like '%' and organising the output for readability

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from stats import *
-
+import sys
 
 #function that reads the file
 def get_book_text(path_file):
@@ -11,13 +11,19 @@ def get_book_text(path_file):
 
 def main():
     # get the file path and calculation from stats.py
-    print("============ BOOKBOT ============ ")
-    print("Analyzing book found at books/frankenstein.txt...")
-    book = get_book_text("books/frankenstein.txt")
+    path_length = len(sys.argv)
+    if path_length > 1:
+        path = sys.argv[1]
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book = get_book_text(path)
     words = book.split()
     num_words = count_words(words)
     characters = char_count(book)
     sort_characters = sort_char(characters)
+    print("============ BOOKBOT ============ ")
+    print("Analyzing book found at", path)
     print("----------- Word Count ----------")
     print("Found",num_words, "total words")
     print("--------- Character Count -------")
